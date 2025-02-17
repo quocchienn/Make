1acb299b120393f5747a936a80dd901655a9825f
Quốc Chiến <123287600+quocchienn@users.noreply.github.com>
2025-02-17 09:54:47 +0700

    Delete Lightroom_Pre.js

--- Lightroom_Pre.js
+++
@@ -1,7 +0,0 @@
-#!name=LightRoom Pro#!desc=tele:quocchienn#!homepage=https://t.me/shadowrocketunlockproapps
-
-[MITM]
-hostname = %APPEND% photos.adobe.io
-
-[Script]
-LightRoom=type=http-response,pattern=^https:\/\/photos\.adobe\.io\/v2\/accounts*,requires-body=1,script-path=https://raw.githubusercontent.com/quocchienn/Make/refs/heads/crack/Lightroom.js
